03e70f1f8201c1b5ac6a674c2b99c4595fc422bc
sammy <dassamaara@gmail.com>
2024-08-09 17:50:10 +0530

    Fixed the problem of game hanging when restarting
    The reason the game hanged was because a valid position for the head of
    the snake couldn't be found. The code was stuck in a forever loop.

--- lib/game_provider.dart
+++ lib/game_provider.dart
@@ -19,6 +19,9 @@ class GameProvider extends ChangeNotifier {
   int score = 0;
   int highScore = 0;
   bool gameOver = false;
+  late Timer moveTimer;
+  late Timer fruitTimer;
+  late Timer gameOverTimer;
 
   GameProvider({required this.columns, required this.rows}) {
     totalSquares = columns * rows;
@@ -48,21 +51,33 @@ class GameProvider extends ChangeNotifier {
     borders = borders.toSet().toList();
   }
 
-  void runChecks() async {
-    // Move the snake in its direction
-    Future.value(Timer.periodic(Duration(milliseconds: 200), (timer) {
+  int _getRandomNumber({required int start, required int end}) {
+    if (start >= end) {
+      throw ArgumentError('Start must be less than end');
+    }
+
+    Random random = Random();
+    return start + random.nextInt(end - start + 1);
+  }
+
+  void runChecks() {
+    moveTimer = Timer.periodic(Duration(milliseconds: 200), (timer) {
       if(!gameOver) changeDirection();
-    }));
+    });
 
-    // Check if the fruit is eaten
-    Future.value(Timer.periodic(Duration(milliseconds: 100), (timer) {
+    fruitTimer = Timer.periodic(Duration(milliseconds: 100), (timer) {
       if(!gameOver) isFruitEaten();
-    }));
+    });
 
-    // Check if the game is over
-    Future.value(Timer.periodic(Duration(milliseconds: 100), (timer) {
+    gameOverTimer = Timer.periodic(Duration(milliseconds: 100), (timer) {
       isGameOver();
-    }));
+    });
+  }
+
+  void cancelTimers() {
+    moveTimer.cancel();
+    fruitTimer.cancel();
+    gameOverTimer.cancel();
   }
 
   /// If the fruit is eaten the fruit re-appears elsewhere, the snake grows and the score and/or high score increases
@@ -95,12 +110,14 @@ class GameProvider extends ChangeNotifier {
 
   /// This resets the game to its initial state
   void resetGame() {
+    cancelTimers();
     score = 0;
     gameOver = false;
     initializeGame();
     currDirection = Direction.Left;
     clickedDirection = Direction.Still;
     notifyListeners();
+    runChecks();
   }
 
   void initializeGame() {
@@ -109,19 +126,47 @@ class GameProvider extends ChangeNotifier {
   }
 
   void initializeFruit(int totalSquares) {
+    int attempts = 0;
     do {
       fruitIndex = _random.nextInt(totalSquares);
+      attempts++;
+      if (attempts > 1000) { // If there have been 1000 attempts, find a position to place the fruit
+        for (int i = 0; i < totalSquares; i++) {
+          if (!snakeBody.contains(i) && !borders.contains(i)) {
+            fruitIndex = i;
+            break;
+          }
+        }
+        break;
+      }
     } while (snakeBody.contains(fruitIndex) || borders.contains(fruitIndex));
   }
-  
+
   void initializeSnake(int totalSquares) {
-    int row = _random.nextInt(rows);
+    int attempts = 0;
+
+    // Select a random row, excluding the first and last rows
+    int row = _getRandomNumber(start: 1, end: rows - 2);
     int startIndex = row * columns;
     int endIndex = startIndex + columns - 1;
 
     do {
-      headIndex = _random.nextInt(endIndex - startIndex - 2) + startIndex;
-    } while (headIndex == fruitIndex || headIndex + 1 == fruitIndex || headIndex + 2 == fruitIndex || borders.contains(headIndex));
+      // Select a random position for the head, avoiding the first and last column
+      headIndex = _getRandomNumber(start: startIndex + 1, end: endIndex - 3);
+      attempts++;
+      if (attempts > 1000) {
+        // If too many attempts, place the snake in the middle of the row
+        headIndex = (startIndex + endIndex) ~/ 2;
+        break;
+      }
+    } while (
+    headIndex == fruitIndex ||
+        headIndex + 1 == fruitIndex ||
+        headIndex + 2 == fruitIndex ||
+        borders.contains(headIndex) ||
+        borders.contains(headIndex + 1) ||
+        borders.contains(headIndex + 2)
+    );
 
     snakeBody = [headIndex, headIndex + 1, headIndex + 2];
   }
